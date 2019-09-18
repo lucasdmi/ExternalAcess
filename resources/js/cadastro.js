a9497c3039eb6723363c914be643c7fd3f6c4591
Lucas Isidoro <lucasmisidoro@gmail.com>
2019-09-18 11:34:09 -0300

    CadastroFinalizado

---
+++ resources/js/cadastro.js
@@ -0,0 +1,6 @@
+
+function confirmacao(){
+ bootbox.alert('Termo de uso: O uso desse meio de comunicação é regulado pela Lei 12.965/14 que consagra todos os direito e deveres no acesso da internet no Brasil. É assegurado o direito a inviolabilidade da intimidade e da vida privada, sua proteção e indenização pelo dano material ou moral decorrente de sua violação (como está na redação do Art.6, I da lei 12.965/14). Esta empresa não se responsabiliza por danos decorrentes de conteúdo gerado por terceiros (conforme redação do Art.18, caput, da lei 12.965/14), bem como sofrerá sanção o usuário que violar qualquer direito autoral, conforme a Lei 9.610/98.')
+   
+    
+}
